f6ebf08e27888b1ee34d453fa9847cdcd9713af5
Dhanshri <dhanshrisapkale5@gmail.com>
2024-11-21 23:46:40 +0530

    for displaying changes in arraylist

--- ExampleforGitHub/src/com/velocity/Example.java
+++ ExampleforGitHub/src/com/velocity/Example.java
@@ -1,5 +1,9 @@
 package com.velocity;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Example {
 	
 	public static void main(String[] args) {
@@ -7,7 +11,22 @@ public class Example {
 		String str = new String("Dhanshri");
 	    System.out.println(str);	
 	     
-	     System.out.println("In Develop branch");
+	    System.out.println("In Develop branch");
+	  
+	    List<String> al = new ArrayList<String>();
+	    
+	    al.add("Ram");
+	    al.add("Shyam");
+	    al.add("Meera");
+	    al.add("Sai");
+	    System.out.println("List is >> "+al);
+	    
+	    Iterator<String> itr = al.iterator();
+	    
+	    while(itr.hasNext())
+	    {
+	      System.out.println(itr.next());
+	    }
 	}
 
 }
