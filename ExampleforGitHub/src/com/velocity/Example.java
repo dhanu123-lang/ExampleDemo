880c3e0cc29162d01851a3203428c7d14f0f5f37
Dhanshri <dhanshrisapkale5@gmail.com>
2024-11-21 23:27:14 +0530

    for develop branch

--- ExampleforGitHub/src/com/velocity/Example.java
+++ ExampleforGitHub/src/com/velocity/Example.java
@@ -5,7 +5,9 @@ public class Example {
 	public static void main(String[] args) {
 		
 		String str = new String("Dhanshri");
-	     System.out.println(str);	
+	    System.out.println(str);	
+	     
+	     System.out.println("In Develop branch");
 	}
 
 }
