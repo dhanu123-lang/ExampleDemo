492a94684897e9d7df57de86a9b8b448778d649f
Dhanshri <dhanshrisapkale5@gmail.com>
2024-11-22 00:04:06 +0530

    display data using for loop

--- ExampleforGitHub/src/com/velocity/Example.java
+++ ExampleforGitHub/src/com/velocity/Example.java
@@ -1,15 +1,18 @@
 package com.velocity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Example {
 	
 	public static void main(String[] args) {
 		
 		String str = new String("Dhanshri");
-	     System.out.println(str);	
+	    System.out.println(str);
+	     
+	    System.out.println("test for change");
 
+	    for(int i =0;i<=5;i++)
+	    {
+	    	System.out.println("Value of i is >> "+i);
+	    }
 	}
 
 }
