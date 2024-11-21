d6a3f5ea2b3c557fe70aaaee9ea6c62d8605a7b6
Dhanshri <dhanshrisapkale5@gmail.com>
2024-11-21 22:57:19 +0530

    Display data in arraylist

---
+++ ExampleforGitHub/src/com/velocity/Example.java
@@ -0,0 +1,11 @@
+package com.velocity;
+
+public class Example {
+	
+	public static void main(String[] args) {
+		
+		String str = new String("Dhanshri");
+	     System.out.println(str);	
+	}
+
+}
---
+++ ExampleforGitHub/src/com/velocity/Sample.java
@@ -0,0 +1,22 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+		
+		System.out.println(list.toString());
+		
+	}
+
+}
