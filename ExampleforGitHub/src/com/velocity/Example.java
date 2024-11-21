b55b67d81890a4aae8446d149ef25bdad61b6520
Dhanshri <dhanshrisapkale5@gmail.com>
2024-11-21 23:56:42 +0530

    for display data

--- ExampleforGitHub/src/com/velocity/Example.java
+++ ExampleforGitHub/src/com/velocity/Example.java
@@ -1,11 +1,15 @@
 package com.velocity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Example {
 	
 	public static void main(String[] args) {
 		
 		String str = new String("Dhanshri");
 	     System.out.println(str);	
+
 	}
 
 }
